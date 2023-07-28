9739db4da328ffb0b6607d9859bbe98ace6ed7e8
dammy995 <damianemerah1@gmail.com>
2023-07-28 13:57:08 +0100

    task 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,7 +4,7 @@
 
 def index_range(page: int, page_size: int) -> tuple[int, int]:
     '''Return a tuple for start and end index'''
-    if page and page_size > 1:
+    if page > 1 and page_size > 1:
         start = page * page_size - page_size
         end = page * page_size
     return (start, end)

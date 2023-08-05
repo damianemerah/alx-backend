cce99e58ae793aa7a4d657f10061a1b0b24f1a8f
dammy995 <damianemerah1@gmail.com>
2023-08-05 15:56:35 +0100

    pycodestyle

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -4,13 +4,11 @@ from flask import Flask, render_template
 from flask_babel import Babel
 
 
-
 class Config(object):
     """ Config class for Babel"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
-
 
 
 app = Flask(__name__)
--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -4,13 +4,11 @@ from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
-
 class Config(object):
     """ Config class for Babel"""
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
-
 
 
 app = Flask(__name__)
@@ -23,6 +21,7 @@ app.url_map.strict_slashes = False
 def get_locale() -> str:
     """Determines the best match with our supported languages"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
 
 @app.route('/')
 def index():

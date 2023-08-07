2a1e978ebb41db658e4acb379398bc3158e0dd02
dammy995 <damianemerah1@gmail.com>
2023-08-07 09:29:04 +0100

    advanced task

---
+++ 0x02-i18n/6-app.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python3
+""" Basic Flask app with Mock logging in"""
+
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+from typing import Union, Dict
+
+
+class Config(object):
+    """ Config class for Babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user() -> Union[Dict, None]:
+    """ Returns a user dictionary or
+        None if the ID cannot be found
+    """
+    try:
+        return users.get(int(request.args.get('login_as')))
+    except Exception:
+        return None
+
+
+@app.before_request
+def before_request():
+    """ Finds a user if any, and set it as a
+        global on flask.g.user
+    """
+    g.user = get_user()
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Determines the best match with our supported languages"""
+    if request.args.get('locale') in app.config['LANGUAGES']:
+        return request.args.get('locale')
+    if g.user and g.user.get('locale') in app.config['LANGUAGES']:
+        return g.user.get('locale')
+    if request.headers.get('locale') in app.config['LANGUAGES']:
+        return request.headers.get('locale')
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """ Returns a string"""
+    return render_template('6-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
---
+++ 0x02-i18n/7-app.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+""" Basic Flask app with Infer appropriate time zone"""
+
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+from typing import Union, Dict
+
+
+class Config(object):
+    """ Config class for Babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+babel = Babel(app)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user() -> Union[Dict, None]:
+    """ Returns a user dictionary or
+        None if the ID cannot be found
+    """
+    try:
+        return users.get(int(request.args.get('login_as')))
+    except Exception:
+        return None
+
+
+@app.before_request
+def before_request():
+    """ Finds a user if any, and set it as a
+        global on flask.g.user
+    """
+    g.user = get_user()
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """Determines the best match with our supported languages"""
+    if request.args.get('locale') in app.config['LANGUAGES']:
+        return request.args.get('locale')
+    if g.user and g.user.get('locale') in app.config['LANGUAGES']:
+        return g.user.get('locale')
+    if request.headers.get('locale') in app.config['LANGUAGES']:
+        return request.headers.get('locale')
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@babel.timezonesselector
+def get_timezone() -> str:
+    ''' Infer appropriate time zone'''
+    if request.args.get('timezone'):
+        return request.args.get('timezone')
+    if g.user and g.user.get('timezone'):
+        return g.user.get('timezone')
+    return app.config['BABEL_DEFAULT_TIMEZONE']
+
+
+@app.route('/')
+def index():
+    """ Returns a string"""
+    return render_template('7-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
